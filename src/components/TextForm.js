bd571d2bd2f2eec472968f55760f68f82c189ff0
Anshu Yadav <87225957+AnshuYadav007@users.noreply.github.com>
2023-07-10 10:53:15 +0530

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -29,6 +29,7 @@ export default function TextForm(props) {
         let text = document.getElementById("myBox");
         text.select();
         navigator.clipboard.writeText(text.value);
+        document.getSelection().removeAllRanges();
         props.showAlert("Text Copied", "success");
 
     }
@@ -38,42 +39,42 @@ export default function TextForm(props) {
         props.showAlert("Extra Spaces Removed", "success");
 
     }
-    const changeDisplay = () => {
-        setStyle("cont2");
-        setStyle("closeDisplay");
-    }
+    // const changeDisplay = () => {
+    //     setStyle("cont2");
+    //     setStyle("closeDisplay");
+    // }
     const handleOnChange = (event) => {
         // console.log("OnChange");
         setText(event.target.value);
     }
     const [text, setText] = useState("");
-    const [style, setStyle] = useState("alert");
-    const [style2] = useState("closebtn");
+    // const [style, setStyle] = useState("alert");
+    // const [style2] = useState("closebtn");
     return (
         <>
             <div className="container" style={{color: props.mode === 'dark'?'white':'black'}}>
-                <h1>{props.heading}</h1>
+                <h2 className='mb-4'>{props.heading}</h2>
                 <div className="my-3">
-                    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8" style={{background: props.mode === 'dark'?'#3e424b':'white', color: props.mode === 'dark'?'white':'black'}}></textarea>
+                    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8" style={{background: props.mode === 'dark'?'rgb(50 66 100)':'white', color: props.mode === 'dark'?'white':'black'}}></textarea>
                 </div>
-                <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
-                <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to LowerCase</button>
-                <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
-                <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy Text</button>
-                <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to UpperCase</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to LowerCase</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
                 {/* <button className="btn btn-primary mx-2" onClick={handleFupClick}>First word Capital</button> */}
             </div>
             <div className="container my-2" style={{color: props.mode === 'dark'?'white':'black'}}>
                 <h1>Your text summary</h1>
-                <div className={style}>
+                {/* <div className={style}>
                     <span className={style2}></span>
                     <span className="closebtn" onClick={changeDisplay}>&times;</span>
                     <strong>Info!</strong> Please input a space in the end of your text to know the correct word count.
-                </div>
-                <p>{text.split(" ").length - 1} words and {text.length} characters</p>
-                <p>{0.008 * text.split(" ").length - 0.008} minutes to read</p>
+                </div> */}
+                <p>{text.split(" ").filter((element)=>{return element.length !==0}).length} words and {text.length} characters</p>
+                <p>{0.008 * text.split(" ").filter((element)=>{return element.length !==0}).length} minutes to read</p>
                 <h3>Preview Text</h3>
-                <p>{text}</p>
+                <p>{text.length>0?text:'Nothing to Preview!'}</p>
             </div>
         </>
     )
